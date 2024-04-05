af5da98a6fe72a840c1aa1829345e0ed275e06f4
rsxyz <rsxyz123@gmail.com>
2024-04-05 14:06:56 -0500

    updated

--- go-gin-hello/README.md
+++ go-gin-hello/README.md
@@ -16,10 +16,27 @@ curl http://localhost:8080
 ```sh
 docker build -t go-gin-hello .
 docker run -d -p 8080:8080 go-gin-hello
+docker run -e "MESSAGE=Good Morning" -e "COLOR=blue" -p 8080:8080 go-gin-hello:latest
+docker run -e "MESSAGE=Good Evening" -e "COLOR=red" -e "USE_TLS=true" -e "TLS_CERT=/certificates/tls.crt" -e "TLS_KEY=/certificates/tls.key" -v $(PWD)/certs:/certificates -p 443:443 go-gin-hello:latest
 
 docker tag go-gin-hello:latest rsxyz123/go-gin-hello:latest
 docker push rsxyz123/go-gin-hello:latest
 docker rmi rsxyz123/go-gin-hello:latest
 docker pull rsxyz123/go-gin-hello:latest
 docker run -e "MESSAGE=Good Morning" -e "COLOR=blue" -p 8080:8080 rsxyz123/go-gin-hello:latest
+docker run -e "MESSAGE=Good Night" -e "COLOR=red" -e "USE_TLS=true" -e "TLS_CERT=/certificates/tls.crt" -e "TLS_KEY=/certificates/tls.key" -v $(PWD)/certs:/certificates -p 443:443 go-gin-hello:latest
+```
+
+# run in kubernetes
+
+```sh
+kubectl create secret generic tls-secret \
+  --from-file=tls.crt \
+  --from-file=tls.key
+
+kubectl apply -f deploy.yaml
+
+kubectl apply -f service.yaml
+sudo kubectl port-forward service/go-gin-service 443:443
+
 ```
--- go-gin-hello/cmd/hello/main.go
+++ go-gin-hello/cmd/hello/main.go
@@ -4,6 +4,10 @@ import (
 	"go-gin-hello/internal/api"
 
 	"github.com/gin-gonic/gin"
+
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -13,6 +17,34 @@ func main() {
 	// Define a route for the root endpoint
 	router.GET("/", api.HelloHandler)
 
-	// Run the server on port 8080
-	router.Run(":8080")
+	// Check if the application is configured to run with TLS
+	if os.Getenv("USE_TLS") == "true" {
+		// Ensure TLS_CERT and TLS_KEY environment variables are set
+		tlsCert := os.Getenv("TLS_CERT")
+		tlsKey := os.Getenv("TLS_KEY")
+		if tlsCert == "" || tlsKey == "" {
+			fmt.Println("TLS_CERT and TLS_KEY environment variables must be provided for HTTPS requests")
+			return
+		}
+
+		// Run the server with TLS configuration
+		go func() {
+			fmt.Println("Listening on port 443...")
+			err := http.ListenAndServeTLS(":443", tlsCert, tlsKey, router)
+			if err != nil {
+				fmt.Printf("Error starting server on port 443: %s\n", err)
+			}
+		}()
+	} else {
+		// Run the server on port 8080 without TLS
+		fmt.Println("Listening on port 8080...")
+		go func() {
+			err := http.ListenAndServe(":8080", router)
+			if err != nil {
+				fmt.Printf("Error starting server on port 8080: %s\n", err)
+			}
+		}()
+	}
+
+	select {}
 }
